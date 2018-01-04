b15d751e5fb27658675c5b3db30b81f575bcb92e
Sarmiento <sarmife@prd.manulifeusa.com>
2018-01-04 15:29:54 -0500

    JWT
    create JWT token in PI01 name as "api/auth/token"

--- PI01/Controllers/HomeController.cs
+++ PI01/Controllers/HomeController.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using PI01.Models;
 
 namespace PI01.Controllers
 {
     public class HomeController : Controller
     {
+        private ILogger<HomeController> _logger;
+
         public IActionResult Index()
         {
             return View();
@@ -32,6 +39,44 @@ namespace PI01.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost("api/auth/token")]
+        public async Task<IActionResult> CreateToken()
+        {
+            try
+            {
+                var user = "mahikero";
+
+                var claims = new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("verulonfdlsjflsfksfjsjkfsjkfjkshflsflsjfl;ksjfioshfohslflkskflgkey"));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "http://mycodecamp.orf",
+                    audience: "http://mycodecamp.orf",
+                    claims: claims,
+                    expires: DateTime.UtcNow.AddMinutes(15),
+                    signingCredentials:  creds
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo
+                });
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Exception thrown while creating JWT: {ex}");
+            }
+
+            return BadRequest ("failed");
         }
     }
 }
