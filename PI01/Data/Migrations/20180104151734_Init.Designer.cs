02a2409cc37b3c9e47ece556135972cfc4e518eb
Sarmiento <sarmife@prd.manulifeusa.com>
2018-01-04 13:46:27 -0500

    up

--- PI01/Data/Migrations/20180104151734_Init.Designer.cs
+++ PI01/Data/Migrations/20180104151734_Init.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace PI01.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180104151734_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
