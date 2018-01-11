f40f7458a18f13b42ae33cbb20c65e669e06f7ae
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-01-11 08:54:35 -0500

    Create a view for signing in. for requesting a  token

--- PC02/Controllers/HomeController.cs
+++ PC02/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PC02.Models;
 
@@ -10,24 +11,48 @@ namespace PC02.Controllers
 {
     public class HomeController : Controller
     {
+        private ISession _session;
+        private string _authToken;
+        private string _userToken;
+
+        public HomeController()
+        {
+            _authToken = _session.GetString("authToken");
+            _userToken = _session.GetString("userToken");
+
+            if (_authToken == null)
+            {
+                //get token for authentication
+                //change route to signIn
+            }
+        }
+
         public IActionResult Index()
         {
+            
+
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult SignIn()
         {
-            ViewData["Message"] = "Your application description page.";
-
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
-            return View();
-        }
+        //public IActionResult About()
+        //{
+        //    ViewData["Message"] = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public IActionResult Contact()
+        //{
+        //    ViewData["Message"] = "Your contact page.";
+
+        //    return View();
+        //}
 
         public IActionResult Error()
         {
