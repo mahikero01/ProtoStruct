384308ae93ee27fc6af0aebbe6bd0c144616a94f
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2018-01-17 13:35:11 -0500

    up

--- PC02/Controllers/HomeController.cs
+++ PC02/Controllers/HomeController.cs
@@ -42,47 +42,7 @@ namespace PC02.Controllers
                 return Redirect("Home/SignIn");
             }
 
-            // test only
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("jjshfkhfkghbvcviutlnvunnksjknvkfjbkrtygbdfg545678566jdkhiufh"));
-            SecurityToken validatedToken;
-            var tokenHandler = new JwtSecurityTokenHandler();
 
-            var tokenValidationParameters = new TokenValidationParameters()
-            {
-                ValidAudiences = new string[]
-                {   "http://mycodecamp.orf"
-                },
-                ValidIssuers = new string[]
-                {
-                    "http://mycodecamp.orf"
-                },
-                IssuerSigningKey = key
-
-            };
-            var claimsPrincipal = tokenHandler.ValidateToken(_authToken, tokenValidationParameters, out validatedToken);
-            var asd = claimsPrincipal.Identity as ClaimsIdentity;
-            //identity.AddClaim(new Claim);
-            System.Diagnostics.Debug.WriteLine(validatedToken.ToString());
-            System.Diagnostics.Debug.WriteLine(asd);
-            System.Diagnostics.Debug.WriteLine(validatedToken.Id);
-            var jwtToken = new JwtSecurityToken(_authToken);
-            System.Diagnostics.Debug.WriteLine(jwtToken.ToString());
-            //var identity = new ClaimsIdentity(HttpContext.User.Identity);
-            System.Diagnostics.Debug.WriteLine(jwtToken.Claims.ToString());
-            var name = jwtToken.Claims
-                .Where(x => x.Type == ClaimTypes.Role 
-                    || x.Type == ClaimTypes.Name
-                    || x.Type == ClaimTypes.Actor).Select(x=>x.Value);
-
-            foreach(var xz in name)
-            {
-                System.Diagnostics.Debug.WriteLine(xz);
-            }
-
-            //foreach(var xz in name)
-            //    System.Diagnostics.Debug.WriteLine(xz.type + " " + xz.value );
-            
-           
             return View();
         }
 
@@ -141,7 +101,7 @@ namespace PC02.Controllers
 
 
             var myToken = new JwtSecurityTokenHandler().WriteToken(token);
-            JwtSecurityToken ax = new JwtSecurityToken();
+            
             HttpContext.Session.SetString("apiToken", myToken);
         }
 
